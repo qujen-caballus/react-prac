efd8b53e710742fcce7c4a683d55d783052e2aed
Kenju MORIYAMA <qujen@KenjunoMacBook-Air.local>
2020-04-21 10:18:57 +0900

    create class and learn component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() 
-  { 
-    return(
-      <React.Fragment>
-        <label htmlFor = "bar">ber</label>
-         <input type = "text" onChange = {() => {console.log("I am clicked")}} />
-      </React.Fragment>
-    )
+// class App extends Component {
+//   render() 
+//   { 
+//     return(
+//       <React.Fragment>
+//         <label htmlFor = "bar">ber</label>
+//          <input type = "text" onChange = {() => {console.log("I am clicked")}} />
+//       </React.Fragment>
+//     )
     
    
-  }
+//   }
+// }
+
+const App = () => 
+{
+return (
+<div> <Cat /></div>
+)
+}
+
+const Cat = () =>
+{
+  return <div>Meow!!</div>
 }
 
 export default App;
