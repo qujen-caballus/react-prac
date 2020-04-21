08a6b9ed21563cc54cc45a02f4418ceae9520ba7
Kenju MORIYAMA <qujen@KenjunoMacBook-Air.local>
2020-04-21 10:45:15 +0900

    learn props typrs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 
@@ -25,8 +26,9 @@ const User = (props) =>
 return <div>Hi, I am {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps={
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age:PropTypes.number
 }
 
 export default App;
